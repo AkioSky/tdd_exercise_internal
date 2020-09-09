4fa11d52576b28762023de514480cdac3907556c
AkioSky <whitebirdinbluesky1990@gmail.com>
2020-09-08 23:41:35 -0400

    view, template

--- xword_data/urls.py
+++ xword_data/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+
+app_name = 'xword_data'
 
 urlpatterns = [
-    path('')
+    path('', views.drill, name='drill'),
+    path('answer/', views.answer, name='answer')
 ]
--- xword_data/views.py
+++ xword_data/views.py
@@ -4,3 +4,8 @@ from .models import Puzzle, Entry, Clue
 
 # Create your views here.
 def drill(request):
+    return render(request, 'xword_data/drill_view.html')
+
+
+def answer(request):
+    return render(request, 'xword_data/answer_view.html')
